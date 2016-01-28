d545bbb07bafc915267540f86a1068134da05b71
William Jeffries <wm.jeffries@gmail.com>
2016-01-28 16:01:29 -0500

    Remove spacing between method name and parens in declarations

--- wu.rb
+++ wu.rb
@@ -5,7 +5,7 @@ require 'date'
 module WU
   extend self
 
-  def current_observation (station)
+  def current_observation(station)
     uri = build_condition_uri(station)
     res = make_request(uri)
 
@@ -13,7 +13,7 @@ module WU
     return toObservation(current)
   end
 
-  def station_history (station, start_time, end_time=nil)
+  def station_history(station, start_time, end_time=nil)
     raise "start_time is not a date" if !start_time.respond_to?(:to_date)
 
     uri = build_history_uri(station, start_time, end_time)
@@ -32,28 +32,28 @@ module WU
     days.map {|day| toObservations(day) }.reduce {|a, c| a + c}
   end
 
-  def format_date (n)
+  def format_date(n)
     n.strftime("%Y%m%d")
   end
 
-  def format_epoch (n)
+  def format_epoch(n)
     Time.at(n).strftime("%Y%m%d")
   end
 
   private
 
 
-  def make_request (uri)
+  def make_request(uri)
     puts uri
     body = Net::HTTP.get(uri)
     JSON.parse(snowdepth(body))
   end
 
-  def build_condition_uri (station)
+  def build_condition_uri(station)
     URI("http://api.wunderground.com/api/606f3f6977348613/conditions/units:english/v:2.0/q/#{station}.json")
   end
 
-  def build_history_uri (station, t1, t2)
+  def build_history_uri(station, t1, t2)
     start_time = format_date(t1)
     end_time = t2 ? format_date(t2) : ""
     puts "#{station} #{start_time} #{end_time}"
@@ -61,11 +61,11 @@ module WU
     URI("http://api.wunderground.com/api/606f3f6977348613/history_#{start_time}#{end_time}/units:english/v:2.0/q/#{station}.json")
   end
 
-  def snowdepth (json)
+  def snowdepth(json)
     json.gsub(/\"snowdepth\": T,/, "")
   end
 
-  def toObservations (day)
+  def toObservations(day)
     raise "day is not a hash" if !day.respond_to?(:{})
 
     observations = day["observations"]
@@ -74,7 +74,7 @@ module WU
     observations.map {|obs| toObservation(obs) }
   end
 
-  def toObservation (obs)
+  def toObservation(obs)
 
     date = obs["date"]
 
